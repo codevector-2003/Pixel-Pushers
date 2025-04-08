8fc3768c2b7247e695a6291b13692dd8ac817bbf
d_.himath <himath.nimpura@gmail.com>
2025-04-08 22:49:25 +0530

    Update api.js
    Update api.js

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -2,7 +2,7 @@ import  axios from 'axios';
 
 //Create an axios instance with a base URL
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:8078',
+    baseURL: 'http://52.140.41.112:80',
 });
 
 //export the axios instance for use in other files
